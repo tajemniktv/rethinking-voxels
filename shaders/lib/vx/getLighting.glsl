0aee88b549a0e16c4516879d2bde95f59fd706ec
gri573 <david.dg.grimm@gmail.com>
2022-10-12 17:43:45 +0200

    better translucent colours

--- shaders/lib/vx/getLighting.glsl
+++ shaders/lib/vx/getLighting.glsl
@@ -66,7 +66,8 @@ vec3[3] getOcclusion(vec3 vxPos, vec3 normal, vec3[3] lightDirs) {
             float dist = max(max(abs(endPos.x - goalPos.x), abs(endPos.y - goalPos.y)), abs(endPos.z - goalPos.z));
             if (dist < 0.512  + ((goalMat == endMat) ? 1.0 : 0.0)) {
                 rayColor.rgb = length(rayColor) < 0.001 ? vec3(1.0) : rayColor.rgb;
-                rayColor.rgb = sqrt(rayColor.rgb);
+                float rayBrightness = max(max(rayColor.r, rayColor.g), rayColor.b);
+                rayColor.rgb /= sqrt(rayBrightness);
                 rayColor.rgb *= clamp(4 - 4 * rayColor.a, 0, 1);
                 occlusion[k] = rayColor.rgb;
             } 
